19c1a1c1969ffa5c2bd5a668173f4c9ed130225c
Ilyes <ilyes.khemakhem@gmail.com>
2019-07-08 14:05:13 +0100

    added a modular iVAE class
    the modular ModularIVAE class accepts as input an object that specifies
    the architecture and the distribution to sample from for each of the
    distributions defining an iVAE model.

--- lib/models.py
+++ lib/models.py
@@ -12,87 +12,6 @@ def weights_init(m):
         nn.init.xavier_uniform_(m.weight.data)
 
 
-class Dist:
-    def __init__(self):
-        pass
-
-    def sample(self, *args):
-        pass
-
-    def log_pdf(self, *args, **kwargs):
-        pass
-
-
-class Normal(Dist):
-    def __init__(self, device='cpu'):
-        super().__init__()
-        self.device = device
-        self.c = 2 * np.pi * torch.ones(1).to(self.device)
-        self._dist = dist.normal.Normal(torch.zeros(1).to(self.device), torch.ones(1).to(self.device))
-
-    def sample(self, mu, v):
-        eps = self._dist.sample(mu.size()).squeeze()
-        scaled = eps.mul(v.sqrt())
-        return scaled.add(mu)
-
-    def log_pdf(self, x, mu, v, reduce=True):
-        """compute the log-pdf of a normal distribution with diagonal covariance"""
-        lpdf = -0.5 * (torch.log(self.c) + v.log() + (x - mu).pow(2).div(v))
-        if reduce:
-            return lpdf.sum(dim=-1)
-        else:
-            return lpdf
-
-    def log_pdf_full(self, x, mu, v):
-        """
-        compute the log-pdf of a normal distribution with full covariance
-        v is a batch of "pseudo sqrt" of covariance matrices of shape (batch_size, d_latent, d_latent)
-        mu is batch of means of shape (batch_size, d_latent)
-        """
-        batch_size, d = mu.size()
-        cov = torch.einsum('bik,bjk->bij', v, v)  # compute batch cov from its "pseudo sqrt"
-        assert cov.size() == (batch_size, d, d)
-        inv_cov = torch.inverse(cov)  # works on batches
-        c = d * torch.log(self.c)
-        # matrix log det doesn't work on batches!
-        _, logabsdets = self._batch_slogdet(cov)
-        xmu = x - mu
-        return -0.5 * (c + logabsdets + torch.einsum('bi,bij,bj->b', [xmu, inv_cov, xmu]))
-
-    def _batch_slogdet(self, cov_batch: torch.Tensor):
-        """
-        compute the log of the absolute value of determinants for a batch of 2D matrices. Uses torch.slogdet
-        this implementation is just a for loop, but that is what's suggested in torch forums
-        gpu compatible
-        """
-        batch_size = cov_batch.size(0)
-        signs = torch.empty(batch_size, requires_grad=False).to(self.device)
-        logabsdets = torch.empty(batch_size, requires_grad=False).to(self.device)
-        for i, cov in enumerate(cov_batch):
-            signs[i], logabsdets[i] = torch.slogdet(cov)
-        return signs, logabsdets
-
-
-class Laplace(Dist):
-    def __init__(self, device='cpu'):
-        super().__init__()
-        self.device = device
-        self._dist = dist.laplace.Laplace(torch.zeros(1).to(self.device), torch.ones(1).to(self.device) / np.sqrt(2))
-
-    def sample(self, mu, b):
-        eps = self._dist.sample(mu.size())
-        scaled = eps.mul(b)
-        return scaled.add(mu)
-
-    def log_pdf(self, x, mu, b, reduce=True):
-        """compute the log-pdf of a laplace distribution with diagonal covariance"""
-        lpdf = -torch.log(2 * b) - (x - mu).abs().div(b)
-        if reduce:
-            return lpdf.sum(dim=-1)
-        else:
-            return lpdf
-
-
 class MLP(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, n_layers, activation='none', slope=.1, device='cpu'):
         super().__init__()
@@ -150,6 +69,198 @@ class MLP(nn.Module):
             else:
                 h = self._act_f[c](self.fc[c](h))
         return h
+
+
+class Dist:
+    def __init__(self):
+        pass
+
+    def sample(self, *args):
+        pass
+
+    def log_pdf(self, *args, **kwargs):
+        pass
+
+
+class Normal(Dist):
+    def __init__(self, device='cpu'):
+        super().__init__()
+        self.device = device
+        self.c = 2 * np.pi * torch.ones(1).to(self.device)
+        self._dist = dist.normal.Normal(torch.zeros(1).to(self.device), torch.ones(1).to(self.device))
+        self.name = 'gauss'
+
+    def sample(self, mu, v):
+        eps = self._dist.sample(mu.size()).squeeze()
+        scaled = eps.mul(v.sqrt())
+        return scaled.add(mu)
+
+    def log_pdf(self, x, mu, v, reduce=True, param_shape=None):
+        """compute the log-pdf of a normal distribution with diagonal covariance"""
+        if param_shape is not None:
+            mu, v = mu.view(param_shape), v.view(param_shape)
+        lpdf = -0.5 * (torch.log(self.c) + v.log() + (x - mu).pow(2).div(v))
+        if reduce:
+            return lpdf.sum(dim=-1)
+        else:
+            return lpdf
+
+    def log_pdf_full(self, x, mu, v):
+        """
+        compute the log-pdf of a normal distribution with full covariance
+        v is a batch of "pseudo sqrt" of covariance matrices of shape (batch_size, d_latent, d_latent)
+        mu is batch of means of shape (batch_size, d_latent)
+        """
+        batch_size, d = mu.size()
+        cov = torch.einsum('bik,bjk->bij', v, v)  # compute batch cov from its "pseudo sqrt"
+        assert cov.size() == (batch_size, d, d)
+        inv_cov = torch.inverse(cov)  # works on batches
+        c = d * torch.log(self.c)
+        # matrix log det doesn't work on batches!
+        _, logabsdets = self._batch_slogdet(cov)
+        xmu = x - mu
+        return -0.5 * (c + logabsdets + torch.einsum('bi,bij,bj->b', [xmu, inv_cov, xmu]))
+
+    def _batch_slogdet(self, cov_batch: torch.Tensor):
+        """
+        compute the log of the absolute value of determinants for a batch of 2D matrices. Uses torch.slogdet
+        this implementation is just a for loop, but that is what's suggested in torch forums
+        gpu compatible
+        """
+        batch_size = cov_batch.size(0)
+        signs = torch.empty(batch_size, requires_grad=False).to(self.device)
+        logabsdets = torch.empty(batch_size, requires_grad=False).to(self.device)
+        for i, cov in enumerate(cov_batch):
+            signs[i], logabsdets[i] = torch.slogdet(cov)
+        return signs, logabsdets
+
+
+class Laplace(Dist):
+    def __init__(self, device='cpu'):
+        super().__init__()
+        self.device = device
+        self._dist = dist.laplace.Laplace(torch.zeros(1).to(self.device), torch.ones(1).to(self.device) / np.sqrt(2))
+        self.name = 'laplace'
+
+    def sample(self, mu, b):
+        eps = self._dist.sample(mu.size())
+        scaled = eps.mul(b)
+        return scaled.add(mu)
+
+    def log_pdf(self, x, mu, b, reduce=True, param_shape=None):
+        """compute the log-pdf of a laplace distribution with diagonal covariance"""
+        if param_shape is not None:
+            mu, b = mu.view(param_shape), b.view(param_shape)
+        lpdf = -torch.log(2 * b) - (x - mu).abs().div(b)
+        if reduce:
+            return lpdf.sum(dim=-1)
+        else:
+            return lpdf
+
+
+class GaussianMLP(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, n_layers, activation, slope, device, fixed_mean=None,
+                 fixed_var=None):
+        super().__init__()
+        self.distribution = Normal(device=device)
+        if fixed_mean is None:
+            self.mean = MLP(input_dim, output_dim, hidden_dim, n_layers, activation=activation, slope=slope,
+                            device=device)
+        else:
+            self.mean = lambda x: fixed_mean * torch.ones(1).to(device)
+        if fixed_var is None:
+            self.log_var = MLP(input_dim, output_dim, hidden_dim, n_layers, activation=activation, slope=slope,
+                               device=device)
+        else:
+            self.log_var = lambda x: np.log(fixed_var) * torch.ones(1).to(device)
+
+    def sample(self, *params):
+        return self.distribution.sample(*params)
+
+    def log_pdf(self, x, *params, **kwargs):
+        return self.distribution.log_pdf(x, *params, **kwargs)
+
+
+    def forward(self, *input):
+        if len(input) > 1:
+            x = torch.cat(input, dim=1)
+        else:
+            x = input[0]
+        return self.mean(x), self.log_var(x).exp()
+
+
+class ModularIVAE(nn.Module):
+    def __init__(self, latent_dim, data_dim, aux_dim, prior=None, decoder=None, encoder=None,
+                 n_layers=3, hidden_dim=50, activation='lrelu', slope=.1, device='cpu', anneal=False):
+        super().__init__()
+
+        self.data_dim = data_dim
+        self.latent_dim = latent_dim
+        self.aux_dim = aux_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.activation = activation
+        self.slope = slope
+        self.anneal_params = anneal
+
+        if prior is None:
+            self.prior = GaussianMLP(aux_dim, latent_dim, hidden_dim, n_layers, activation=activation, slope=slope,
+                                     device=device, fixed_mean=0)
+        else:
+            self.prior = prior
+
+        if decoder is None:
+            self.decoder = GaussianMLP(latent_dim, data_dim, hidden_dim, n_layers, activation=activation, slope=slope,
+                                       device=device, fixed_var=.01)
+        else:
+            self.decoder = decoder
+
+        if encoder is None:
+            self.encoder = GaussianMLP(data_dim + aux_dim, latent_dim, hidden_dim, n_layers, activation=activation,
+                                       slope=slope, device=device)
+        else:
+            self.encoder = encoder
+
+        self.apply(weights_init)
+
+        self._training_hyperparams = [1., 1., 1., 1., 1]
+
+    def forward(self, x, u):
+        encoder_params = self.encoder(x, u)
+        z = self.encoder.sample(*encoder_params)
+        decoder_params = self.decoder(z)
+        prior_params = self.prior(u)
+        return decoder_params, encoder_params, prior_params, z
+
+    def elbo(self, x, u):
+        decoder_params, encoder_params, prior_params, z = self.forward(x, u)
+        log_px_z = self.decoder.log_pdf(x, *decoder_params)
+        log_qz_xu = self.encoder.log_pdf(z, *encoder_params)
+        log_pz_u = self.prior.log_pdf(z, *prior_params)
+
+        if self.anneal_params:
+            a, b, c, d, N = self._training_hyperparams
+            M = z.size(0)
+            log_qz_tmp = self.encoder.log_pdf(z.view(M, 1, self.latent_dim), encoder_params, reduce=False,
+                                              param_shape=(1, M, self.latent_dim))
+            log_qz = torch.logsumexp(log_qz_tmp.sum(dim=-1), dim=1, keepdim=False) - np.log(M * N)
+            log_qz_i = (torch.logsumexp(log_qz_tmp, dim=1, keepdim=False) - np.log(M * N)).sum(dim=-1)
+
+            return (a * log_px_z - b * (log_qz_xu - log_qz) - c * (log_qz - log_qz_i) - d * (
+                    log_qz_i - log_pz_u)).mean(), z
+        else:
+            return (log_px_z + log_pz_u - log_qz_xu).mean(), z
+
+    def anneal(self, N, max_iter, it):
+        thr = int(max_iter / 1.6)
+        a = 0.5 / self.decoder.log_var(0).exp().item()
+        self._training_hyperparams[-1] = N
+        self._training_hyperparams[0] = min(2 * a, a + a * it / thr)
+        self._training_hyperparams[1] = max(1, a * .3 * (1 - it / thr))
+        self._training_hyperparams[2] = min(1, it / thr)
+        self._training_hyperparams[3] = max(1, a * .5 * (1 - it / thr))
+        if it > thr:
+            self.anneal_params = False
 
 
 class iVAE(nn.Module):
